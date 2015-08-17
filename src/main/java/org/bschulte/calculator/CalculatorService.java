570455ed1ed627c8facd4f88783d9bf0b2859595
Berthold Schulte <bschulte@digitalriver.com>
2015-08-17 13:32:21 +0200

    URL decode question

--- src/main/java/org/bschulte/calculator/CalculatorService.java
+++ src/main/java/org/bschulte/calculator/CalculatorService.java
@@ -1,9 +1,14 @@
 package org.bschulte.calculator;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -18,9 +23,15 @@ public class CalculatorService {
 	@RequestMapping("/")
 	String home(@RequestParam(name = "q", required = true, defaultValue = "") String query) throws UnknownQuestionException {
 		if (StringUtils.isEmpty(query)) {
-			return "Provide a question in parameter: q";
+			return "Please provide a question in parameter: q";
 		}
-		return classifier.calculate(query);
+		String q = null;
+		try {
+			q = URLDecoder.decode(query,"UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			
+		}
+		return classifier.calculate(q);
 	}
 
 	@ExceptionHandler(UnknownQuestionException.class)
